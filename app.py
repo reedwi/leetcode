be2583aa61daf08fabda25f31abc1c3bd61c5f7c
reedwi <65463278+reedwi@users.noreply.github.com>
2021-11-19 09:54:08 -0500

    again

--- app.py
+++ app.py
@@ -428,3 +428,33 @@ class DeleteDuplicatesSolution:
 
 # A = DeleteDuplicatesSolution()
 # A.deleteDuplicates(head=ListNode([1,1,2]))
+
+class MinStartSolution:
+    def minStartValue(self, A: List[int]) -> int:
+        total = 0
+        res = 0
+        for x in A:
+            total = total + x
+            res = min(res, total)
+        return -res + 1
+
+# A = MinStartSolution()
+# A.minStartValue(A=[1,2,3,4,-1])
+
+'''
+448. Find All Numbers Disappeared in an Array
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+'''
+class FindDisappearedNumbersSolution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        return list(set(range(1, len(nums)+1)).difference(set(nums)))
+        return [i for i in range(1, len(nums)+1) if i not in nums]
+
+# A = FindDisappearedNumbersSolution()
+# A.findDisappearedNumbers(nums=[1, 1])
+
